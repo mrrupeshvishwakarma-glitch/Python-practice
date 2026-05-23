43e002bb4f79f93c723faa8517275bb51a1c98f2
Rupesh_Vishwakarma <mrrupeshvishwakarma@gmail.com>
2026-05-23 10:39:04 +0530

    finish intermediate conditions section

--- Conditions/Intermediate Conditions/login_warning.py
+++ Conditions/Intermediate Conditions/login_warning.py
@@ -1,4 +1,14 @@
-attempts = int(input("Enter login attempts: = "))
+attempts = 0
+
+while attempts < 3:
+    password = input("Enter password: = ")
+
+    if password == "admin123":
+        print("Login successful")
+        break
+    else:
+        attempts += 1
+        print("Wrong password")
 
 if attempts == 3:
-    print("Warning!")
+    print("Warning! Too many login attempts")
